f017815e79649838e0da04460a1a99d3c9faa038
22-0202c <22-0202c@CCL01-13>
2023-11-14 12:47:40 +0800

    Add project files.

---
+++ Queueue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> patients = new Queue<string>();
+            patients.Enqueue("Jojo");
+            patients.Enqueue("Tobi Lou");
+            patients.Enqueue("EDP445");
+            Console.WriteLine();
+            foreach (var patient in patients)
+            {
+                Console.WriteLine("\n" + patient);
+            }
+
+            patients.Dequeue();
+            Console.WriteLine("\nfirst has been removed...");
+            foreach (var patient in patients)
+            {
+                Console.WriteLine("\n" + patient);
+            }
+            Console.WriteLine("\nHere is the first person: " + patients.Peek());
+            Console.ReadKey();
+        }
+    }
+}
